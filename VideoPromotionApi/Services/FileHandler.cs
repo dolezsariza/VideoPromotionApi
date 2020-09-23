d55ab7f4078aae959113a96d4cedcd907b377365
Izabella Dolezsár <izabella.dolezsar@gmail.com>
2020-09-23 19:35:25 +0200

    Create FileHandler to read from txt

---
+++ VideoPromotionApi/Services/FileHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoPromotionApi.Services
+{
+    public class FileHandler
+    {
+        public string[] ReadFromFile()
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"VideoPromotionApi\..\..\..\Assets\API.txt");
+
+            return lines;
+        }
+    }
+}
