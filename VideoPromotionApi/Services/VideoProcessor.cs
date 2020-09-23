a3bff793089e59399cd46fd7f851d4a1f3266778
Izabella Dolezsár <izabella.dolezsar@gmail.com>
2020-09-23 19:47:57 +0200

    Create VideoProcessor

---
+++ VideoPromotionApi/Services/VideoProcessor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using VideoPromotionApi.Models;
+
+namespace VideoPromotionApi.Services
+{
+    public class VideoProcessor
+    {
+        public string Url { get; set; }
+        public ApiHelper ApiHelper { get; set; }
+        public VideoProcessor(ApiHelper apiHelper, string url)
+        {
+            ApiHelper = apiHelper;
+            Url = url;
+        }
+        public async Task<VideoModel> LoadVideo()
+        {
+            using (HttpResponseMessage response = await ApiHelper.ApiClient.GetAsync(Url))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    VideoModel video = await response.Content.ReadAsAsync<VideoModel>();
+                    return video;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+    }
+}
