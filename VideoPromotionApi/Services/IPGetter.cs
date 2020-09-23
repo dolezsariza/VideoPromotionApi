720d76b545acfbe7b7a775c772ca4389a6688c2b
Izabella Dolezsár <izabella.dolezsar@gmail.com>
2020-09-23 19:40:26 +0200

    Create IPGetter

---
+++ VideoPromotionApi/Services/IPGetter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoPromotionApi.Services
+{
+    public class IPGetter
+    {
+        public string GetIpOfHost()
+        {
+            string hostName = Dns.GetHostName();
+            string hostIP = Dns.GetHostEntry(hostName).AddressList[1].ToString();
+            return hostIP;
+        }
+    }
+}
