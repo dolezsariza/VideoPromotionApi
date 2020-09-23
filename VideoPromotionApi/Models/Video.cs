9ecf0ca66701db35a548c14430f8eb695ad2f295
Izabella Dolezsár <izabella.dolezsar@gmail.com>
2020-09-23 19:03:06 +0200

    Create models for Json data

---
+++ VideoPromotionApi/Models/Video.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace VideoPromotionApi.Models
+{
+    public class Video
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public int Duration { get; set; }
+        public List<string> Tags { get; set; }
+        public string ProfileImage { get; set; }
+        public string ThumbImage { get; set; }
+        public List<string> PreviewImages { get; set; }
+        public string Quality { get; set; }
+        public string Uploader { get; set; }
+    }
+}
