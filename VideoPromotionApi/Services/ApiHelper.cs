8f31adad49366df8af97ece7dcef1f942e34ee73
Izabella Dolezsár <izabella.dolezsar@gmail.com>
2020-09-23 18:59:45 +0200

    Create ApiHelper

---
+++ VideoPromotionApi/Services/ApiHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace VideoPromotionApi.Services
+{
+    public class ApiHelper
+    {
+        public HttpClient ApiClient { get; set; }
+        public ApiHelper(string url)
+        {
+            ApiClient = new HttpClient();
+            ApiClient.BaseAddress = new Uri(url);
+            ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiClient.DefaultRequestHeaders.Accept
+                .Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
