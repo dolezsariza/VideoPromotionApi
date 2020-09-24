10f986f3aa62d699bc64217780800dc1e46578ca
Izabella Dolezsár <izabella.dolezsar@gmail.com>
2020-09-24 15:57:12 +0200

    Create Display to store video description and thumbnail

---
+++ VideoPromotionApi/Models/Display.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace VideoPromotionApi.Models
+{
+    public class Display
+    {
+        public BitmapImage VideoImage { get; set; }
+        public string Title { get; set; }
+        public int Duration { get; set; }
+        public string Quality { get; set; }
+        public string Uploader { get; set; }
+        public List<string> Tags { get; set; }
+
+        public Display(BitmapImage videoImage, string title, int duration, string quality, string uploader, List<string> tags)
+        {
+            VideoImage = videoImage;
+            Title = title;
+            Duration = duration;
+            Quality = quality;
+            Uploader = uploader;
+            Tags = tags;
+        }
+    }
+}
