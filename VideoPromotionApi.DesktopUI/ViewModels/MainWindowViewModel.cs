9349b0a17c7e23f97627d239156a53dcf79fedbf
Izabella Dolezsár <izabella.dolezsar@gmail.com>
2020-09-25 12:01:04 +0200

    Move logic from MainWindow to MainWindowViewModel

---
+++ VideoPromotionApi.DesktopUI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using VideoPromotionApi.Models;
+using VideoPromotionApi.Services;
+
+namespace VideoPromotionApi.DesktopUI.ViewModels
+{
+    public class MainWindowViewModel
+    {
+        public string UserName { get; set; }
+        public string API_KEY { get; set; }
+        public string IPAddress { get; set; }
+        public List<Display> DataToShow { get; set; }
+        public ApiHelper ApiHelper { get; set; }
+        public VideoProcessor VideoProcessor { get; set; }
+        public IPGetter IPGetter { get; set; }
+        public FileHandler FileHandler { get; set; }
+        private string[] textData;
+        private string baseUrl;
+
+        public MainWindowViewModel()
+        {
+            baseUrl = "https://pt.potawe.com/api/video-promotion/v1/list";
+            ApiHelper = new ApiHelper(baseUrl);
+            FileHandler = new FileHandler();
+            IPGetter = new IPGetter();
+            DataToShow = new List<Display>();
+            textData = FileHandler.ReadFromFile();
+            UserName = textData[0];
+            API_KEY = textData[1];
+            IPAddress = IPGetter.GetIpOfHost();
+            var url = $"{baseUrl}?psid={UserName}&accessKey={API_KEY}&clientIp={IPAddress}";
+            VideoProcessor = new VideoProcessor(ApiHelper, url);
+        }
+        public async Task LoadVideo()
+        {
+            var videoModel = await VideoProcessor.LoadVideo();
+            foreach (var video in videoModel.Data.Videos)
+            {
+                var thumbnailLink = $"https:{video.PreviewImages[0]}";
+                var uriSource = new Uri(thumbnailLink, UriKind.Absolute);
+                var data = new Display(new BitmapImage(uriSource),
+                    video.Title,
+                    video.Duration,
+                    video.Quality,
+                    video.Uploader,
+                    video.Tags);
+                data.Tags.Sort();
+                DataToShow.Add(data);
+            }
+        }
+    }
+}
--- VideoPromotionApi.DesktopUI/Views/MainWindow.xaml.cs
+++ VideoPromotionApi.DesktopUI/Views/MainWindow.xaml.cs
@@ -12,8 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using VideoPromotionApi.Models;
-using VideoPromotionApi.Services;
+using VideoPromotionApi.DesktopUI.ViewModels;
+
 
 namespace VideoPromotionApi.DesktopUI
 {
@@ -22,55 +22,19 @@ namespace VideoPromotionApi.DesktopUI
     /// </summary>
     public partial class MainWindow : Window
     {
-        public string UserName { get; set; }
-        public string API_KEY { get; set; }
-        public string IPAddress { get; set; }
-        public List<Display> DataToShow { get; set; }
-        public ApiHelper ApiHelper { get; set; }
-        public VideoProcessor VideoProcessor { get; set; }
-        public IPGetter IPGetter { get; set; }
-        public FileHandler FileHandler { get; set; }
-        private string[] textData;
-        private string baseUrl;
+        private MainWindowViewModel _mwvm;
 
         public MainWindow()
         {
             InitializeComponent();
-            baseUrl = "https://pt.potawe.com/api/video-promotion/v1/list";
-            ApiHelper = new ApiHelper(baseUrl);
-            FileHandler = new FileHandler();
-            IPGetter = new IPGetter();
-            DataToShow = new List<Display>();
-            textData = FileHandler.ReadFromFile();
-            UserName = textData[0];
-            API_KEY = textData[1];
-            IPAddress = IPGetter.GetIpOfHost();
-            var url = $"{baseUrl}?psid={UserName}&accessKey={API_KEY}&clientIp={IPAddress}";
-            VideoProcessor = new VideoProcessor(ApiHelper, url);
-            DataContext = this;
+            _mwvm = new MainWindowViewModel();
+            DataContext = _mwvm;
         }
 
-        private async Task LoadVideo()
-        {
-            var videoModel = await VideoProcessor.LoadVideo();
-            foreach (var video in videoModel.Data.Videos)
-            {
-                var thumbnailLink = $"https:{video.PreviewImages[0]}";
-                var uriSource = new Uri(thumbnailLink, UriKind.Absolute);
-                var data = new Display(new BitmapImage(uriSource),
-                    video.Title,
-                    video.Duration,
-                    video.Quality,
-                    video.Uploader,
-                    video.Tags);
-                data.Tags.Sort();
-                DataToShow.Add(data);
-            }
-        }
         private async void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            await LoadVideo();
-            videoDataList.ItemsSource = DataToShow;
+            await _mwvm.LoadVideo();
+            videoDataList.ItemsSource = _mwvm.DataToShow;
         }
     }
 }
