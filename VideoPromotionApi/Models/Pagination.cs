15905abcf42dad8a44431b2eabd2ce378dd9e55e
Izabella Dolezsár <izabella.dolezsar@gmail.com>
2020-09-26 11:01:49 +0200

    Add INotifyPropertyChanged to Pagination

--- VideoPromotionApi/Models/Pagination.cs
+++ VideoPromotionApi/Models/Pagination.cs
@@ -1,11 +1,34 @@
-﻿namespace VideoPromotionApi.Models
+﻿using System.ComponentModel;
+
+namespace VideoPromotionApi.Models
 {
-    public class Pagination
+    public class Pagination : INotifyPropertyChanged
     {
         public int Total { get; set; }
         public int Count { get; set; }
-        public int CurrentPage { get; set; }
+		private int currentPage;
+        public int CurrentPage 
+		{
+			get { return currentPage; }
+			set
+			{
+				if (currentPage != value)
+				{
+					currentPage = value;
+					NotifyPropertyChanged("CurrentPage");
+				}
+			}
+		}
         public int PerPage { get; set; }
         public int TotalPages { get; set; }
-    }
+		
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public void NotifyPropertyChanged(string propName)
+		{
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+	}
+
 }
