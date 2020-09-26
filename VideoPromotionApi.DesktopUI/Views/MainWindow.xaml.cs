b4f1f3b613365234393e55fd210e9c57dfc5de82
Izabella Dolezsár <izabella.dolezsar@gmail.com>
2020-09-26 17:12:06 +0200

    Add design with Mahapps.Metro

--- VideoPromotionApi.DesktopUI/Views/MainWindow.xaml.cs
+++ VideoPromotionApi.DesktopUI/Views/MainWindow.xaml.cs
@@ -5,23 +5,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 using VideoPromotionApi.DesktopUI.ViewModels;
-using VideoPromotionApi.Services;
+using MahApps.Metro.Controls;
+using System.Diagnostics;
 
 namespace VideoPromotionApi.DesktopUI
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         private MainWindowViewModel _mwvm;
 
