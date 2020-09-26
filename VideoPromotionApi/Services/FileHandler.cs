acf4f031c50b83ad81f563e3524a569263320191
Izabella Dolezsár <izabella.dolezsar@gmail.com>
2020-09-26 16:02:06 +0200

    Add error handling to FileHandler

--- VideoPromotionApi/Services/FileHandler.cs
+++ VideoPromotionApi/Services/FileHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +11,30 @@ namespace VideoPromotionApi.Services
     {
         public string[] ReadFromFile()
         {
-            string[] lines = System.IO.File.ReadAllLines(@"..\..\..\VideoPromotionApi\Assets\API.txt");
+            string[] lines = new string[] { "psId","accessKey" };
+            try
+            {
+                lines = File.ReadAllLines(@"..\..\..\VideoPromotionApi\Assets\API.txt");
+            } 
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine(@"File not found. Please use Assets\API.txt to provide PSID and access key.");
+            }
 
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine("The file or directory cannot be found.");
+            }
+            catch (DriveNotFoundException)
+            {
+                Console.WriteLine("The drive specified in 'path' is invalid.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
             return lines;
         }
+
     }
 }
