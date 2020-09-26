4c6ddd456494fd87b5c6e9f461a4dfb665c9ae99
Izabella Dolezsár <izabella.dolezsar@gmail.com>
2020-09-26 13:05:53 +0200

    Add methods to handle click events of navigation buttons

--- VideoPromotionApi.DesktopUI/Views/MainWindow.xaml.cs
+++ VideoPromotionApi.DesktopUI/Views/MainWindow.xaml.cs
@@ -30,6 +30,7 @@ namespace VideoPromotionApi.DesktopUI
             InitializeComponent();
             _mwvm = new MainWindowViewModel();
             DataContext = _mwvm;
+
         }
 
         private async void Window_Loaded(object sender, RoutedEventArgs e)
@@ -39,31 +40,53 @@ namespace VideoPromotionApi.DesktopUI
             Pagination.DataContext = _mwvm.Pagination;
         }
 
-        private async void ChooseFilter_Click(object sender, RoutedEventArgs e)
+        private async void ChooseFilter_Clicked(object sender, RoutedEventArgs e)
         {
             var quality = Quality.Text.ToString().ToLower();
             var uploader = Uploader.Text.ToString();
             await _mwvm.FilterVideo(quality, uploader);
-            
+            Pagination.DataContext = _mwvm.Pagination;
+            firstButton.IsEnabled = previousButton.IsEnabled = false;
+
         }
 
-        private void goToPreviousPage(object sender, RoutedEventArgs e)
+        private async void GoToFirstPage(object sender, RoutedEventArgs e)
         {
-
+            await _mwvm.ChangePage("first");
+            Pagination.DataContext = _mwvm.Pagination;
+            previousButton.IsEnabled = firstButton.IsEnabled = false;
+            nextButton.IsEnabled = lastButton.IsEnabled = true;
         }
 
-        private void goToNextPage(object sender, RoutedEventArgs e)
+        private async void GoToPreviousPage(object sender, RoutedEventArgs e)
         {
-
+            await _mwvm.ChangePage("previous");
+            Pagination.DataContext = _mwvm.Pagination;
+            if (_mwvm.Pagination.CurrentPage == 1)
+            {
+                previousButton.IsEnabled = firstButton.IsEnabled = false;
+            }
+            nextButton.IsEnabled = lastButton.IsEnabled = true;
         }
 
-        private void goToFirstPage(object sender, RoutedEventArgs e)
+        private async void GoToNextPage(object sender, RoutedEventArgs e)
         {
-
+            await _mwvm.ChangePage("next");
+            Pagination.DataContext = _mwvm.Pagination;
+            if (_mwvm.Pagination.CurrentPage == _mwvm.Pagination.TotalPages)
+            {
+                previousButton.IsEnabled = lastButton.IsEnabled = false;
+            }
+            firstButton.IsEnabled = previousButton.IsEnabled = true;
         }
-        private void goToLastPage(object sender, RoutedEventArgs e)
+
+        private async void GoToLastPage(object sender, RoutedEventArgs e)
         {
-
+            await _mwvm.ChangePage("last");
+            Pagination.DataContext = _mwvm.Pagination;
+            firstButton.IsEnabled = previousButton.IsEnabled = true;
+            nextButton.IsEnabled = lastButton.IsEnabled = false;
         }
+
     }
 }
