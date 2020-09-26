6542e92d765ba6be3b6ce366319eadb30b448a62
Izabella Dolezsár <izabella.dolezsar@gmail.com>
2020-09-26 13:06:04 +0200

    Remove unused code

--- VideoPromotionApi/Models/Pagination.cs
+++ VideoPromotionApi/Models/Pagination.cs
@@ -2,33 +2,14 @@
 
 namespace VideoPromotionApi.Models
 {
-    public class Pagination : INotifyPropertyChanged
+    public class Pagination 
     {
         public int Total { get; set; }
         public int Count { get; set; }
-		private int currentPage;
-        public int CurrentPage 
-		{
-			get { return currentPage; }
-			set
-			{
-				if (currentPage != value)
-				{
-					currentPage = value;
-					NotifyPropertyChanged("CurrentPage");
-				}
-			}
-		}
+        public int CurrentPage { get; set; }
         public int PerPage { get; set; }
         public int TotalPages { get; set; }
 		
-
-		public event PropertyChangedEventHandler PropertyChanged;
-
-		public void NotifyPropertyChanged(string propName)
-		{
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
-        }
 	}
 
 }
